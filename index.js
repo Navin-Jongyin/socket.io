cffb63460c519c8f21821196981ca410722dd2d3
ynp44 <ext.yanapat.emd@truedigital.com>
2024-04-04 07:14:48 +0700

    kuay

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+const { log } = require("console");
 const express = require("express");
-const {createServer} = require ("http");
-const {Server} = require("socket.io");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-app.route("/").get((req,res) =>{
+app.route("/").get((req, res) => {
     res.json("Hello")
 });
 
-io.on("connection",(socket) => {
+io.on("connection", (socket) => {
+    socket.join("Kuay_Group");
+    console.log("backend connected");
+    socket.on("sendMsg", (msg) => { //recieve message from front
+        console.log("message is", msg);
+        // socket.emit("sendMsgServer", { ...msg, type: "otherMsg" });
+        io.to("Kuay_Group").emit("sendMsgServer", { ...msg, type: "otherMsg" });
+    })
 
 });
 
